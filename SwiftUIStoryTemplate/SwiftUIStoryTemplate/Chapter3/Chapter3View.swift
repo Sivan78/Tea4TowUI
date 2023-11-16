3f9d7db7b5f12b1fcc1d3d8db11ef8782a5eee4a
Adryenne <dreamy26@icloud.com>
2023-11-16 09:54:06 -0500

    chore: adds test code

--- SwiftUIStoryTemplate/SwiftUIStoryTemplate/Chapter3/Chapter3View.swift
+++ SwiftUIStoryTemplate/SwiftUIStoryTemplate/Chapter3/Chapter3View.swift
@@ -3,6 +3,14 @@ import SwiftUI
 struct Chapter3View: View {
     var body: some View {
         Text("Chapter 3")
+        
+        ZStack{
+            Circle()
+                           .foregroundColor(.purple)
+                           .frame(width: 100, height: 100, alignment: .center)
+                       Text("Hello, World!")
+                   }
+                   .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
